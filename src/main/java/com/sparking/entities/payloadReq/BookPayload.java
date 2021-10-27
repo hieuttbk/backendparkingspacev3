50b693648c065bb25f9dca4f7c78a4406b583748
hongdatchy <hongdatchy@gmail.com>
2021-10-27 15:37:21 +0700

    mn find add: fix respone

--- src/main/java/com/sparking/entities/payloadReq/BookPayload.java
+++ src/main/java/com/sparking/entities/payloadReq/BookPayload.java
@@ -11,8 +11,8 @@ public class BookPayload {
     private Integer fieldId;
     private String carNumber;
 //    vì default timezone đang là GMT+7 rồi nên không viết timezone = "GMT+7" cũng vẫn được
-    @JsonFormat(pattern="yyyy-MM-dd HH:mm", timezone = "GMT+7")
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm", timezone = "GMT+7")
     private Timestamp timeInBook;
-    @JsonFormat(pattern="yyyy-MM-dd HH:mm")
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm")
     private Timestamp timeOutBook;
 }
