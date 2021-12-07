822bbc21ae45d8b4e4daa7a7ead45cddbd2f32f2
htnicayh <hieuhp1306@gmail.com>
2021-12-07 21:47:07 +0700

    change pattern datetime

--- src/main/java/com/sparking/entities/payloadReq/ManUpdateInfoPayload.java
+++ src/main/java/com/sparking/entities/payloadReq/ManUpdateInfoPayload.java
@@ -13,10 +13,8 @@ public class ManUpdateInfoPayload {
     private String image;
     private String address;
     private String sex;
-    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm")
+
+    @JsonFormat(pattern="yyyy-MM-dd")
     private Date birth;
-
-
-
 
 }
